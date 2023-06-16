e494cf26bb4ef96b988cdec671e86bcd1bbaca76
PC-20230502HZON\Administrator <1974524707@qq.com>
2023-06-16 15:34:39 +0800

    更新华佗到3.2

---
+++ Unity/Assets/Scripts/Codes/ModelView/Client/Demo/UI/DlgItemPopUp/DlgItemPopUp.cs
@@ -0,0 +1,12 @@
+﻿namespace ET.Client
+{
+	 [ComponentOf(typeof(UIBaseWindow))]
+	public  class DlgItemPopUp :Entity,IAwake,IUILogic
+	{
+
+		public DlgItemPopUpViewComponent View { get => this.GetParent<UIBaseWindow>().GetComponent<DlgItemPopUpViewComponent>();} 
+
+		 
+
+	}
+}
